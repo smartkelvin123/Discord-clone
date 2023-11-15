027f24afb9a77256c175bb813c2334333ee71721
unknown <smartkelvin66@gmail.com>
2023-11-15 13:49:05 +0100

    smart

---
+++ server/models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  username: String,
+  password: String,
+});
+
+module.exports = mongoose.model("User", userSchema);
