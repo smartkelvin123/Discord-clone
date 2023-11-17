91df8db91511ffcb22016404670837b964ddee5f
unknown <smartkelvin66@gmail.com>
2023-11-17 05:31:17 +0100

    redux

--- discord-app/src/store/store.js
+++ discord-app/src/store/store.js
@@ -12,3 +12,23 @@ const store = configureStore({
 });
 
 export default store;
+
+// import { composeWithDevTools } from "redux-devtools-extension";
+
+// import { applyMiddleware, combineReducers } from "redux";
+// import { configureStore } from "@reduxjs/toolkit";
+// import thunk from "redux-thunk";
+// import authReducer from "./reducers/authReducers";
+
+// const rootReducer = combineReducers({
+//   auth: authReducer,
+// });
+
+// could you updated for me
+
+// const store = configureStore(
+//   rootReducer,
+//   composeWithDevTools(applyMiddleware(thunk))
+// );
+
+// export default store;
