241997d228ac688f099adbce1267bbd302744145
unknown <smartkelvin66@gmail.com>
2023-11-28 21:50:43 +0100

    updated

--- discord-app/src/dashboard/friendsSidebar/friendlist/friendListItem.js
+++ discord-app/src/dashboard/friendsSidebar/friendlist/friendListItem.js
@@ -2,34 +2,34 @@ import React from "react";
 import { Button, Typography } from "@mui/material";
 import Avatar from "../../../shared/component/avatar";
 
-const FriendListItem = ({ id, userName, isOnline }) => {
+const FriendListItem = ({ id, username, isOnline }) => {
   return (
     <Button
       style={{
         width: "100%",
         height: "42px",
         marginTop: "10px",
-        color: "black",
-        backgroundColor: "#5865F2",
         display: "flex",
-        justifyContent: "flex-start",
         alignItems: "center",
+        justifyContent: "flex-start",
         textTransform: "none",
+        color: "black",
         position: "relative",
       }}
     >
-      <Avatar userName={userName} />
+      <Avatar username={username} />
       <Typography
         style={{
           marginLeft: "7px",
-          fontSize: "700",
+          fontWeight: 700,
           color: "#8e9297",
         }}
         variant="subtitle1"
         align="left"
       >
-        {userName}
+        {username}
       </Typography>
+      {/* {isOnline && <OnlineIndicator />} */}
     </Button>
   );
 };
