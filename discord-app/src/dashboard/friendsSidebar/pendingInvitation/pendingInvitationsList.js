1ce1dc69a1f7595c3c03a66e340ffba2016bd4ad
unknown <smartkelvin66@gmail.com>
2023-11-29 14:15:12 +0100

    update pendinginvite

--- discord-app/src/dashboard/friendsSidebar/pendingInvitation/pendingInvitationsList.js
+++ discord-app/src/dashboard/friendsSidebar/pendingInvitation/pendingInvitationsList.js
@@ -1,6 +1,23 @@
 import React from "react";
 import { styled } from "@mui/material";
 
+const DUMMY_INVITATIONS = [
+  {
+    _id: "1",
+    senderId: {
+      username: "Mark",
+      mail: "dummy@ad.com",
+    },
+  },
+  {
+    _id: "2",
+    senderId: {
+      username: "John",
+      mail: "John@ad.com",
+    },
+  },
+];
+
 const MainContainer = styled("div")({
   width: "100%",
   height: "22%",
@@ -12,7 +29,16 @@ const MainContainer = styled("div")({
 });
 
 const PendingInvitation = () => {
-  return <MainContainer></MainContainer>;
+  <MainContainer>
+    {DUMMY_INVITATIONS.map((invitation) => (
+      <PendingInvitationsListItem
+        key={invitation._id}
+        id={invitation._id}
+        username={invitation.senderId.username}
+        mail={invitation.senderId.mail}
+      />
+    ))}
+  </MainContainer>;
 };
 
 export default PendingInvitation;
